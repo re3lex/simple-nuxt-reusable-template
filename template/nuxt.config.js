1792833214dc5abf1f4ede6def4f8fefe3299d4c
Alexey Ulyanov <lexxsol@yandex.ru>
2019-07-03 21:49:32 +0400

    initial commit

---
+++ template/nuxt.config.js
@@ -0,0 +1,55 @@
+
+export default {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/style-resources',
+  ],
+  styleResources: {
+    scss: [
+      'assets/scss/_var.scss',
+    ],
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    extractCSS: true,
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
